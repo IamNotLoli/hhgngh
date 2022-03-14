daf522bf88fd0668cedc6dc42875bfeda32269ab
IamNotLoli <101261570+IamNotLoli@users.noreply.github.com>
2022-03-14 17:49:39 +0500

    Add files via upload

---
+++ src/Components/Portfolio/PorfolioMenu/PortfolioAbout.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../../../App.css'
+
+export const PortfolioAbout = () => {
+  return (
+    <div className='Aib'>
+        <h5>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis itaque officiis accusantium tempora quasi fugiat ea molestiae harum est atque? Nobis maiores exercitationem deleniti ex quam, nesciunt porro earum iste.</h5>
+    </div>
+  )
+}
